6f1410210d17b5ec37d1e0bef79f794ba5486dc5
c.lamarche.50 <charles.lamarche.1@ulaval.ca>
2024-06-29 14:59:23 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['sans-serif'],
+    },
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
